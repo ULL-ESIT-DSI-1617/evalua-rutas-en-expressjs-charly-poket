268891f868b314b1c5f7b8f4c40b8b774e4a0890
charly-poket <alu0100844665@ull.edu.es>
2017-03-13 20:55:17 +0100

    Cambio en la organizacion del repo y pruebas con views en ejs

--- server/myserver.js
+++ server/myserver.js
@@ -2,7 +2,9 @@ var express = require('express')
 var app = express()
 var path = require('path');
 var router = express.Router();
+//var users = require('../routes/user');
 
+//app.use('/users', users);
 module.exports = router;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -10,9 +12,23 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
- // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 })
+
+app.get('/public', function (req, res) {
+
+  res.send('Got a GET request'+'<br/><img src="smile.png" />');
+
+})
+
+app.get('/user/:id?', function (req, res) {
+  console.log('req.params.id: '+(req.params.id));
+  console.log('Request Type:', req.method);
+  console.log('Request Path:', req.path);
+  //res.send('USUARIO: '+(req.params.id || 'unknown' ));
+  res.render('index', { title: '/user/' ,username: req.params.id });
+});
+
 var server = app.listen(8084, function () {
 
   var host = server.address().address
